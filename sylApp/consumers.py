b198f75b6050bdd2bce24067e1f7e9e11f81b3b5
Bhavya Kundra <bhavyaku.tws@gmail.com>
2022-07-26 16:39:35 +0530

    Function Updated

--- sylApp/consumers.py
+++ sylApp/consumers.py
@@ -6,13 +6,15 @@ from asgiref.sync import sync_to_async
 from asgiref.sync import async_to_sync
 import random
 from channels.layers import get_channel_layer
-from sylApp.main import thullu
+from sylApp.main import thullu , user_card_return
+import json
 
 
 def cardDistribute(player):
 
    totalCards = []
-   givenCardUnicode = ["♣","♦","♥","♠"]
+   givenCardUnicode = ["♣","'♦'","♥","♠"]
+   turn = 0
 
    for i in range(len(givenCardUnicode)):
        for j in range(2,15):
@@ -24,9 +26,13 @@ def cardDistribute(player):
    totalNumberOfCard = 52
    for j in range(player):
 
-       singlePlayer = {"command":"your_cards" , "cards":[]}
+       singlePlayer = {"command":"your_cards" , "cards":[],"first_turn":""}
        for k in range(totalNumberOfCard//player):
            givenvalue = random.choice(totalCards)
+           if givenvalue=="14♠":
+              print(j,"gddddddddddddddddddddddddddddddddddddddddddddddddddd")
+              turn  = j
+              singlePlayer["first_turn"]=j
            L = len(givenvalue)
            singlePlayer["cards"].append({'number':givenvalue[:-1], 'symbol':givenvalue[L-1]})
            totalCards.remove(givenvalue)
@@ -37,6 +43,9 @@ def cardDistribute(player):
        L = len(givenvalue)
        allPlayers[i+1]["cards"].append({'number':givenvalue[:-1], 'symbol':givenvalue[L-1]})
        totalCards.remove(givenvalue)
+
+   for b in range(player):
+        allPlayers[b]["first_turn"]=turn
    return allPlayers
 
 
@@ -66,6 +75,7 @@ class SYLConsumer(AsyncWebsocketConsumer):
 
     messages = []
     room = {}
+    all_players_cards = {}
 
     def join_chat(self,username):
         key = self.room_id
@@ -126,7 +136,7 @@ class SYLConsumer(AsyncWebsocketConsumer):
         await self.accept()
         self.join_chat(self.user_id),
         return await self.channel_layer.group_send(
-                self.user_group_name,
+                self.room_group_name,
                 {
                 'type': 'chat_message',  
                 "message":{
@@ -134,7 +144,7 @@ class SYLConsumer(AsyncWebsocketConsumer):
                             "user": [{
                             "id": self.user_id,
                             "username":username,
-                            # "profile_pic": "url",
+                            # "all_user":self.room[self.room_id],
                             "status": "watchig",
                          }
                         ]
@@ -174,17 +184,24 @@ class SYLConsumer(AsyncWebsocketConsumer):
         cards = cardDistribute(len(user_ids))
         print(text_data,"-----------------------------------------------------")
         text_data_json = text_data
-        print(text_data_json,"--------------------------------------")
+        print(type(text_data_json),"--------------------------------------")
         username = self.scope['url_route']['kwargs']['user_id']
         print(username,"--------------------------------------")
-        message = text_data_json
-        print(message,"message")
-        if message == "distribute":
+        message = json.loads(text_data_json)
+        print(type(message),"fdfdfdffdfdfdfdfdfdffdfdf")
+        if message["command"]== "distribute":
             user_ids = self.room[key]
-            print("--------------------------------------", len(user_ids))
+            print(user_ids,"1111111111111111")
             cards = cardDistribute(len(user_ids))
+            print(cards,"cardscardscardscardscardscardscardscardscards")
             for i in range(len(user_ids)):
-                print(cards[i],"222")
+                if len(self.all_players_cards) <1:
+                    self.all_players_cards[key]={user_ids[i]:cards[i]['cards']}
+                else:
+                      self.all_players_cards[key][user_ids[i]]=cards[i]['cards']
+                card=cards[i]
+                ele=card['first_turn']
+                card['first_turn']=user_ids[int(ele)]
                 self.user_group_name = '_%s' % user_ids[i]
                 await self.channel_layer.group_send(
                 self.user_group_name,
@@ -194,18 +211,16 @@ class SYLConsumer(AsyncWebsocketConsumer):
                 "username": user_ids[i],
                 "profile_pic": "url",
                 "status": "playing",
-                "message": cards[i]
+                "message": card
                 }
                 )
-              
-        elif message == "turn":
-            turn = thullu(cards)
-            print(turn)
+        elif message["command"] == "my_play":
+            reply = user_card_return(message["card"] , self.all_players_cards, message["user_data"]["user_id"] , message["user_data"]["room_id"],user_ids)
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
                 'type': 'chat_message',
-                "message": turn
+                "message": reply
                 }
                 )
 
--- sylApp/main.py
+++ sylApp/main.py
@@ -1,4 +1,7 @@
 import random
+
+all_user_play={}
+
 def restriction(l1,last_card):
     newlist=[]
     for i in l1:
@@ -7,87 +10,123 @@ def restriction(l1,last_card):
     if len(newlist)==0:
         return l1
     else:
-        return newlist
+        return newlist 
+
+# def turn(allplayer_card):
+#     print(allplayer_card,"8888888888888888888888888888888888888888888888888888888888888888888888")
+#     for i in range(len(allplayer_card[1])):
+#        for j in range(len(allplayer_card[1])):
+#             if allplayer_card[1][j][0]=='14' and allplayer_card[1][j][1]=="♠":
+#                 return {'player':j+1}
 
 
-def thullu(allplayers):
-   card = []
-   print(allplayers,"sdsdssdd")
-   turn_starting_player = 0
-   temp1 = 0 
-   for i in range(len(allplayers)):
-       for j in range(len(allplayers)):
-            if "14♠" in allplayers[j]:
-                return {'player':j+1}
-       if i==0:
-           card = []
-       if len(card)>=1:
-           restriction(allplayers[i], card[len(card)-1])
-       givencard=str(input(f"Player{i+1}:enter the card you want to display: "))
-       card.append(givencard)
-       allplayers[i].remove(givencard)
-       if i >= 1:
-           last_card = card[i-1]
-           if givencard[len(givencard)-1] != last_card[len(last_card)-1] :
-               if len(card)==2:
-                   allplayers[0].extend(card)
-                   return allplayers ,{'player':turn_starting_player+1}
-               else:
-                   for j in range(len(card)):
-                       a=card[j]
-                       if  int(temp1) < int(a[slice(-1)]):
-                           temp1 = a[slice(-1)]
-                           turn_starting_player = j
-                   allplayers[turn_starting_player].extend(card)
-               return allplayers ,{'player':turn_starting_player+1}
-           else:
-                if len(card)==len(allplayers):
-                    for j in range(len(card)):
-                        a=card[j]
-                        if  int(temp1) < int(a[slice(-1)]):
-                            temp1 = a[slice(-1)]
-                            turn_starting_player = j
-                    return {'player':turn_starting_player+1}
-        
-             
+# user_play , user_cards , user_id , all_user_id
+turns = 0
+def update_turn(user_id,all_user_play ,key_list,all_user_cards , room_id):
+    all_user_play ={31:{'number':'1' , 'symbol':"♠"},32:{'number':'2' , 'symbol':"♠"}, 
+    30:{'number': '12', 'symbol': '♥'}}
+    all_user_id=[30,31,32]
+    user_id = user_id
+    turn=0
+    global turns
+    turns=turns+1
+    turn_starting_player = 0
+    key_list = [31,32,30] 
+    # working on this "function"
+    if turns ==1 :
+        for i in range(len(key_list)):
+           for j in range(0,int(5/52)+1):
+                if all_user_cards[room_id][key_list[j]]['number']=='14' and all_user_cards[room_id][key_list[j]]['symbol']=="♠":
+                    return {'player':j+1}
 
-                
+    if turns == len(all_user_id):
+        for k in range(len(all_user_play)):
+                a=all_user_play[key_list[k]]
+                if  int(temp1) < int(a['number']):
+                    temp1 = a['number']
+                    turn_starting_player = key_list[k]
+
+    for i in range(len(all_user_id)):
+        if all_user_id[i]==user_id:
+            turn_index = i
+            if len(all_user_id)==i+1:
+                if turns<len(all_user_id):
+                    turn = all_user_id[0]
+            else:
+                if turns<len(all_user_id):
+                    turn = all_user_id[i+1]
+    print(turn,"turn")
 
 
-   # card = []
-   # allplayers = allSortedCard
-   # i = 0
-   # temp = 0
-   # running = True
-   # while running ==  True:
 
-   #     for i in range(len(allplayers)):
-   #         givencard=str(input("Player:enter the card you want to display: "))
-   #         card.append(givencard)
-   #         allplayers[i].remove(givencard)
-   #         if i >= 1:
-   #             c = card[i-1]
-   #             if givencard[len(givencard)-1] != c[len©-1] :
-   #                 if len(card)==2:
-   #                     allplayers[0].extend(card)
-   #                     break
-   #                 else:
-   #                     for j in range(len(card)):
-   #                         if j >=1:
-   #                             a=card[j]
-   #                             b=card[j-1]
-   #                             if  int(b[0]) < int(a[0]):
-   #                                 temp = j
-   #                             else:
-   #                                 temp = j-1
+all_user_play = {} 
+def user_card_return(user_play , all_user_cards, user_id, room_id,all_user_id):
+    user_play=user_play
+    all_user_cards =all_user_cards
+    room_id = room_id
+    user_id = user_id
+    all_user_id=all_user_id
+    
+    global all_user_play
+    # temp1 = 0 
+    # turn_starting_player=0
+    for i in range(len(all_user_cards[room_id][user_id])):
+        if all_user_cards[room_id][user_id][i]['number']==user_play['number'] and user_play['symbol']==all_user_cards[room_id][user_id][i]['symbol']:
+            all_user_play[user_id]=user_play
+            all_user_cards[room_id][user_id].remove(user_play)
+            print(all_user_cards)
+            return(all_user_cards)
+            # user_cards.remove(user_cards[i])
+            # if len(all_user_play)>1:
+            #     thullu(user_play , all_user_cards,room_id,all_user_id)
+            # else:
+            #     update_turn(all_user_id,all_user_cards)
+            # # print(user_cards)
+            # break
 
-   #                 allplayers[temp].extend(card)
-   #                 card =[]
-   #                 break
-   #     print(allplayers)
-   #     if len(allplayers)==1:
-   #         running = False
 
+def thullu(user_play,all_user_cards,room_id,all_user_id):
+    global all_user_play
+    all_user_cards = all_user_cards
+    all_user_id  = all_user_id
+    room_id = room_id
+    key_list = []
+    temp1 = 0
+    turn_starting_player = 0
+    for key in all_user_play.keys():
+        key_list.append(key)
+    last_card = all_user_play[key_list[len(all_user_play)-2]]
+    print(last_card)
+    if last_card['symbol']!= user_play['symbol']:
+        if len(all_user_play)==2:
+            for i in range(len(key_list)):
+                if all_user_play[key_list[i]] not in  all_user_cards[room_id][key_list[len(all_user_play)-2]]:
+                    all_user_cards[room_id][key_list[len(all_user_play)-2]].append(all_user_play[key_list[i]])
+            all_user_play = {}
+
+        # check bigger card
+        else:
+            for k in range(len(all_user_play)):
+                a=all_user_play[key_list[k]]
+                if  int(temp1) < int(a['number']):
+                    temp1 = a['number']
+                    turn_starting_player = key_list[k]
+            print(turn_starting_player,"sdsdsdsd")
+            for d in range(len(key_list)):
+                if all_user_play[key_list[d]] not in  all_user_cards[room_id][turn_starting_player]:
+                    all_user_cards[room_id][turn_starting_player].append(all_user_play[key_list[d]])
+            all_user_play = {}
+
+    # saar
+    else:
+        update_turn(all_user_id)
+    if len(all_user_id)==len(all_user_play):
+        turn=update_turn(all_user_play , key_list)
+        all_user_play = {}
+
+    # for j in  range(len(all_user_id)):
+    #     if user_id == all_user_id[i]:
+    #         return all_user_id[d+1]
 
 
 
@@ -180,3 +219,65 @@ if __name__ == "__main__":
     main()
     # print(cardDistribute(2))
     #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def thullu(allplayers):
+#    card = []
+#    print(allplayers,"sdsdssdd") 
+#    turn_starting_player = 0
+#    temp1 = 0 
+#    for i in range(len(allplayers)):
+#        for j in range(len(allplayers)):
+#             if "14♠" in allplayers[j]:
+#                 return {'player':j+1}
+#        if i==0:
+#            card = []
+#        if len(card)>=1:
+#            restriction(allplayers[i], card[len(card)-1])
+#        givencard=str(input(f"Player{i+1}:enter the card you want to display: "))
+#        card.append(givencard)
+#        allplayers[i].remove(givencard)
+#        if i >= 1:
+#            last_card = card[i-1]
+#            if givencard[len(givencard)-1] != last_card[len(last_card)-1] :
+#                if len(card)==2:
+#                    allplayers[0].extend(card)
+#                    return allplayers ,{'player':turn_starting_player+1}
+#                else:
+#                    for j in range(len(card)):
+#                        a=card[j]
+#                        if  int(temp1) < int(a[slice(-1)]):
+#                            temp1 = a[slice(-1)]
+#                            turn_starting_player = j
+#                    allplayers[turn_starting_player].extend(card)
+#                return allplayers ,{'player':turn_starting_player+1}
+#            else:
+#                 if len(card)==len(allplayers):
+#                     for j in range(len(card)):
+#                         a=card[j]
+#                         if  int(temp1) < int(a[slice(-1)]):
+#                             temp1 = a[slice(-1)]
+#                             turn_starting_player = j
+#                     return {'player':turn_starting_player+1}
+#         # return playerturn
+             
