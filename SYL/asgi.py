674b6a08d00b7f3b3dd02d0cc0efd49583fb19cf
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-07-29 14:49:24 +0530

    c

--- SYL/asgi.py
+++ SYL/asgi.py
@@ -6,18 +6,19 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
-
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SYL.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from sylApp import routing
-import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SYL.settings')
-django.setup()
+
 
 application = ProtocolTypeRouter(
     {
