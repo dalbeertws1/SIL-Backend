6a83f1f938af9bc1daac34d330b3ecee3593612c
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2022-07-29 12:48:17 +0530

    update setting file

--- SYL/settings.py
+++ SYL/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 import os
 from pathlib import Path
 
@@ -75,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-# WSGI_APPLICATION = 'SYL.wsgi.application'
+WSGI_APPLICATION = 'SYL.wsgi.application'
 
 ASGI_APPLICATION = 'SYL.asgi.application'
 
@@ -151,3 +151,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
